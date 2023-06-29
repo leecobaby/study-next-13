fe8cc1c4d99e6a9ed5fc15142e8652234b77b504
Leeco <leeco19@qq.com>
2023-06-29 21:27:17 +0800

    P3-2: Implement base navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,58 @@
+'use client'
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { styled } from 'styled-components'
 
 export function Navbar() {
   return (
-    <>
-      <div>Navigation</div>
-    </>
+    <Warp>
+      <section className="logo">BLOG-C</section>
+      <section className="link">
+        {navs?.map(item => (
+          <Link key={item.label} href={item.harf}>
+            {item.label}
+          </Link>
+        ))}
+      </section>
+    </Warp>
   )
 }
+
+const navs = [
+  {
+    label: '首页',
+    harf: '/',
+  },
+  {
+    label: '资讯',
+    harf: '/info',
+  },
+  {
+    label: '标签',
+    harf: '/tag',
+  },
+]
+
+/** style */
+
+const Warp = styled.div`
+  height: 60px;
+  background-color: aqua;
+  border-bottom: 1px solid #f1f1f1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .logo {
+    font-size: 30px;
+    font-weight: bold;
+    margin-right: 60px;
+  }
+
+  .link {
+    & > * {
+      font-size: 18px;
+      padding: 0 20px;
+    }
+  }
+`
